05d163430e838787594f1e87e47f7d1dc302d7ce
Harshil Patel <26harshilpatel11@gmail.com>
2024-05-10 11:56:28 -0400

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.commonjs,
+        ...globals.es2021,
+        ...globals.node,
+      },
+    },
+  },
+];
