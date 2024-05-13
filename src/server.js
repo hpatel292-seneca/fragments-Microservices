fd18fc782c90b86e3f227d5a1cf0e95037d1ce11
Harshil Patel <26harshilpatel11@gmail.com>
2024-05-13 13:45:46 -0400

    added logic to log environment variables

--- src/server.js
+++ src/server.js
@@ -17,6 +17,9 @@ const server = stoppable(
   app.listen(port, () => {
     // Log a message that the server has started, and which port it's using.
     logger.info(`Server started on port ${port}`);
+    if (process.env.LOG_LEVEL === 'debug') {
+      logger.info(process.env);
+    }
   })
 );
 
