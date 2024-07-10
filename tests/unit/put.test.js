2c812e1de638f63d53c3b56db1e240cd391293d3
Harshil Patel <26harshilpatel11@gmail.com>
2024-07-10 16:21:52 -0400

    added unit test for successful PUT request

---
+++ tests/unit/put.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const hash = require('../../src/hash');
+const { Fragment } = require('../../src/model/fragment');
+
+describe('PUT /v1/fragments/:id', () => {
+  // Should update specified fragment for given id with given new fragment
+  test('Should update specified fragment for given id with given new fragment', async () => {
+    const ownerId = hash('user1@email.com');
+    const id = 'rdmId';
+    const fragMetadata = new Fragment({ id: id, ownerId: ownerId, type: 'text/plain' });
+    const body = 'This is a fragment';
+    const updated_body = 'This is updated fragment';
+    fragMetadata.setData(body);
+    fragMetadata.save();
+
+    const res = await request(app)
+      .put(`/v1/fragments/${id}`)
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send(updated_body);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
+    expect(res.body.fragment.id).toBe(id);
+
+    // fetch updated body.
+    const newData = await fragMetadata.getData();
+    expect(newData.toString('utf-8')).toBe(updated_body);
+  });
+
+  // // Should throw if fragment didn't exist
+  // test("Should throw if fragment didn't exist", async () => {
+  //   const id = 'rdmId';
+
+  //   const res = await request(app)
+  //     .delete(`/v1/fragments/${id}`)
+  //     .auth('user1@email.com', 'password1');
+  //   expect(res.statusCode).toBe(404);
+  // });
+});
