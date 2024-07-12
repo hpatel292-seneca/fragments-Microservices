0681e098b8105c36860aeac1a4f9b99234fbd629
Harshil Patel <26harshilpatel11@gmail.com>
2024-07-12 13:45:27 -0400

    remove saperate file for test case for /fragment/:id and integrate it into get.test.js

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const hash = require('../../src/hash');
 const app = require('../../src/app');
 const { Fragment } = require('../../src/model/fragment');
+const markdownit = require('markdown-it');
 
 describe('GET /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
@@ -38,21 +39,20 @@ describe('GET /v1/fragments', () => {
 describe('/v1/fragments?expand=1', () => {
   // fetching correct fragments with passed expand=1 as query parameter
   test('fetching correct fragments when passed expand=1', async () => {
+    const ownerId = hash('user1@email.com');
+    const id = 'rdmId';
     const type = 'text/plain';
-    const res = await request(app)
-      .post('/v1/fragments')
-      .auth('user1@email.com', 'password1')
-      .set('Content-Type', type)
-      .send('This is a fragment');
-    const id = res.body.fragment.id;
+    const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: type });
+    const body = 'This is a fragment';
+    fragMetadata1.setData(body);
+    fragMetadata1.save();
 
-    const res_2 = await request(app)
+    const res = await request(app)
       .get('/v1/fragments?expand=1')
       .auth('user1@email.com', 'password1');
-    // [1] because post request has been made in above test.
-    expect(res_2.body.fragments[1].id).toBe(id);
-    expect(res_2.body.fragments[1].ownerId).toBe(hash('user1@email.com'));
-    expect(res_2.body.fragments[1].type).toBe(type);
+    expect(res.body.fragments[0].id).toBe(id);
+    expect(res.body.fragments[0].ownerId).toBe(hash('user1@email.com'));
+    expect(res.body.fragments[0].type).toBe(type);
   });
 });
 
@@ -119,5 +119,114 @@ describe('GET /v1/fragments/:id/info', () => {
 
     // Restore the original method
     require('../../src/model/fragment').Fragment.byId = originalById;
+  });
+});
+
+// Get specific fragments
+describe('GET /v1/fragments/:id', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () =>
+    request(app).get('/v1/fragments/ecdca9b2-b841-47e5-be4d-7f880d3c8c59').expect(401));
+
+  // If the wrong username/password pair are used (no such user), it should be forbidden
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .get('/v1/fragments/ecdca9b2-b841-47e5-be4d-7f880d3c8c59')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
+
+  // throw when no fragment for given ID
+  test('should return 404 If fragment not found', async () => {
+    const res = await request(app).get('/v1/fragments/1234').auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(404);
+  });
+
+  // return specific fragment data without post request.
+  test('return specific fragment data without post request', async () => {
+    const ownerId = hash('user1@email.com');
+    const id = 'rdmId';
+    const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: 'text/plain' });
+    const body = 'This is a fragment';
+    fragMetadata1.setData(body);
+    fragMetadata1.save();
+
+    const res = await request(app).get(`/v1/fragments/${id}`).auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe(body);
+  });
+
+  // return specific fragment data
+  test('return specific fragment data', async () => {
+    const ownerId = hash('user1@email.com');
+    const id = 'rdmId';
+    const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: 'text/plain' });
+    const body = 'This is a fragment';
+    fragMetadata1.setData(body);
+    fragMetadata1.save();
+
+    const res = await request(app).get(`/v1/fragments/${id}`).auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe(body);
+  });
+});
+
+describe('GET /v1/fragments/:id.ext', () => {
+  // should return fragment data successfully with extension
+  test('should return fragment data with extension', async () => {
+    // post a fragment
+    const body = 'This is a fragment';
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send(body);
+    const id = res.body.fragment.id;
+
+    const res_2 = await request(app)
+      .get(`/v1/fragments/${id}.txt`)
+      .auth('user1@email.com', 'password1');
+    expect(res_2.statusCode).toBe(200);
+    expect(res_2.text).toBe(body);
+  });
+
+  // should return markdown fragments to html.
+  test('should return markdown fragments to html', async () => {
+    const body = '# Heading level 1';
+    const ownerId = hash('user1@email.com');
+    const id = 'rdmId';
+    const type = 'text/markdown';
+    const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: type });
+    fragMetadata1.setData(body);
+    fragMetadata1.save();
+    const md = markdownit();
+    const result = md.render(body);
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe(result);
+  });
+  // should return 415 if unsupported extension is requested
+  test('should return 415 if unsupported extension is requested', async () => {
+    // post a fragment
+    const body = 'This is a fragment';
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send(body);
+    const id = res.body.fragment.id;
+
+    const res_2 = await request(app)
+      .get(`/v1/fragments/${id}.png`)
+      .auth('user1@email.com', 'password1');
+    expect(res_2.statusCode).toBe(415);
+    expect(res_2.body).toEqual({
+      status: 'error',
+      error: {
+        code: 415,
+        message: 'The fragment cannot be converted into the extension specified!',
+      },
+    });
   });
 });
