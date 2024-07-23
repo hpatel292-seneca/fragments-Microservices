d0798c1ea33ca5be632c0cafdee00f4d904d2c66
Harshil Patel <26harshilpatel11@gmail.com>
2024-07-23 12:51:28 -0400

    update data/index.js to configure aws if region is persent in env variable or in-memory

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,1 +1,5 @@
-module.exports = require('./memory');
+// src/model/data/index.js
+
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
