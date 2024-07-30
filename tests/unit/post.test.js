0b58403673db3ed664daa5f6f3c4e9a59e411d2e
Harshil Patel <26harshilpatel11@gmail.com>
2024-07-30 12:41:52 -0400

    added unit test for posting image/jpeg fragment

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -95,6 +95,19 @@ describe('Post /v1/fragments', () => {
     expect(res.headers['location']).toMatch(/\/v1\/fragments\/[a-f0-9-]+$/);
   });
 
+  // Authenticated user can create image/jpeg fragments
+  test('authenticated user can create image/jpeg fragments and location must returned in header', async () => {
+    const fileContent = 'This is a test file content';
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'image/jpeg')
+      .send(Buffer.from(fileContent));
+    expect(res.statusCode).toBe(201);
+    expect(res.body.status).toBe('ok');
+    expect(res.headers['location']).toMatch(/\/v1\/fragments\/[a-f0-9-]+$/);
+  });
+
   // response include all necessary and expected properties
   test('post return fragment with all necessary properties', async () => {
     const data = 'This is a fragment';
