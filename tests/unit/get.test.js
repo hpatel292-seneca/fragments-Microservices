394b69138a59773d32153dd4085666d9b2c5abeb
Harshil Patel <26harshilpatel11@gmail.com>
2024-08-01 17:42:57 -0400

    added unit test for converting markdown fragment to html

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -190,25 +190,6 @@ describe('GET /v1/fragments/:id.ext', () => {
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe(body);
   });
-
-  // should return markdown fragments to html.
-  test('should return markdown fragments to html', async () => {
-    const body = '# Heading level 1';
-    const ownerId = hash('user1@email.com');
-    const id = 'rdmId';
-    const type = 'text/markdown';
-    const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: type });
-    fragMetadata1.setData(body);
-    fragMetadata1.save();
-    const md = markdownit();
-    const result = md.render(body);
-
-    const res = await request(app)
-      .get(`/v1/fragments/${id}.html`)
-      .auth('user1@email.com', 'password1');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe(result);
-  });
   // should return 415 if unsupported extension is requested
   test('should return 415 if unsupported extension is requested', async () => {
     // post a fragment
@@ -685,6 +666,28 @@ describe('GET /v1/fragments/:id.ext', () => {
 
       expect(receivedMetadata).toEqual(originalMetadata);
       expect(Buffer.compare(receivedFileContent, fileContent)).toBe(0);
+    });
+  });
+
+  describe('Converted Fragments should be fetched successfully', () => {
+    test('Markdown fragment should successfully converted to HTML', async () => {
+      // post a fragment
+      const filePath = path.join(__dirname, '..', 'files', 'file.md');
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const ownerId = hash('user1@email.com');
+      const id = 'rdmId';
+      const type = 'text/markdown';
+      const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: type });
+      fragMetadata1.setData(fileContent);
+      fragMetadata1.save();
+      const md = markdownit();
+      const result = md.render(fileContent);
+
+      const res = await request(app)
+        .get(`/v1/fragments/${id}.html`)
+        .auth('user1@email.com', 'password1');
+      expect(res.statusCode).toBe(200);
+      expect(res.text).toBe(result);
     });
   });
 });
