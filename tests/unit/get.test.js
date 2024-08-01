e87d6cd6dfa235179497d51ef320a07e98008c6e
Harshil Patel <26harshilpatel11@gmail.com>
2024-08-01 18:11:18 -0400

    added unit test for converting yaml fragment to txt

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -838,4 +838,23 @@ describe('GET /v1/fragments/:id.ext', () => {
       expect(res.text).toEqual(fileContent);
     });
   });
+
+  describe('YAML Fragments should be converted successfully', () => {
+    test('YAML fragment should successfully converted to .txt', async () => {
+      // post a fragment
+      const filePath = path.join(__dirname, '..', 'files', 'file.yaml');
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const ownerId = hash('user1@email.com');
+      const id = 'rdmId';
+      const type = 'application/yaml';
+      const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: type });
+      fragMetadata1.setData(fileContent);
+      fragMetadata1.save();
+      const res = await request(app)
+        .get(`/v1/fragments/${id}.txt`)
+        .auth('user1@email.com', 'password1');
+      expect(res.statusCode).toBe(200);
+      expect(res.text).toEqual(fileContent);
+    });
+  });
 });
