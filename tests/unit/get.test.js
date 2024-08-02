f42e8dfee6e9cc663919c2198cc591107bcfe2d0
Harshil Patel <26harshilpatel11@gmail.com>
2024-08-02 13:08:13 -0400

    added unit test for converting jpeg fragment to webp

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -980,5 +980,29 @@ describe('GET /v1/fragments/:id.ext', () => {
       expect(receivedMetadata.format).toBe('png');
       expect(Buffer.compare(receivedFileContent, originalContent)).toBe(0);
     });
+
+    test('JPEG fragments data should converted to .webp', async () => {
+      // post a fragment
+      const filePath = path.join(__dirname, '..', 'files', 'file.jpeg');
+      const fileContent = fs.readFileSync(filePath);
+      const ownerId = hash('user1@email.com');
+      const id = 'rdmId';
+      const type = 'image/jpeg';
+      const fragMetadata1 = new Fragment({ id: id, ownerId: ownerId, type: type });
+      fragMetadata1.setData(fileContent);
+      fragMetadata1.save();
+
+      const res = await request(app)
+        .get(`/v1/fragments/${id}.webp`)
+        .auth('user1@email.com', 'password1');
+      expect(res.statusCode).toBe(200);
+      const receivedFileContent = res.body;
+
+      const receivedMetadata = await sharp(receivedFileContent).metadata();
+      const originalContent = await sharp(fileContent).webp().toBuffer();
+      // Sharp is returning Avif as heif
+      expect(receivedMetadata.format).toBe('webp');
+      expect(Buffer.compare(receivedFileContent, originalContent)).toBe(0);
+    });
   });
 });
